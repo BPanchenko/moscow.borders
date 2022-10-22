ba9aa45910e730f87f7b3fc1425ad6d6d6be66cb
Boris Panchenko <oberon@list.ru>
2022-10-22 15:32:34 +0300

    initial import of useful modules

---
+++ core/main.mjs
@@ -0,0 +1,4 @@
+import "protosite-core/custom-elements/button.js"
+import "protosite-core/custom-elements/field.js"
+import "protosite-core/custom-elements/grid"
+import "protosite-core/lib/utilites"
