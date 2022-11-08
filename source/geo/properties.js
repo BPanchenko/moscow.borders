fc6f65c65b07006af08d568b2049d9e16cc06560
Boris Panchenko <oberon@list.ru>
2022-11-08 23:56:09 +0300

    regions decomposed into separate files

--- source/geo/properties.js
+++ source/geo/properties.js
@@ -1,6 +1,363 @@
 export default {
-	ru: {
-		code: 'RU',
-		name: 'Российская Федерация'
+	'ru': {
+	  code: 'RU',
+	  name: 'Российская Федерация'
+	},
+	'ru-alt': {
+	  code: 'RU-ALT',
+	  name: 'Алтайский край'
+	},
+	'ru-amu': {
+	  code: 'RU-AMU',
+	  name: 'Амурская область'
+	},
+	'ru-ark': {
+	  code: 'RU-ARK',
+	  name: 'Архангельская область'
+	},
+	'ru-ast': {
+	  code: 'RU-AST',
+	  name: 'Астраханская область'
+	},
+	'ru-bel': {
+	  code: 'RU-BEL',
+	  name: 'Белгородская область'
+	},
+	'ru-bry': {
+	  code: 'RU-BRY',
+	  name: 'Брянская область'
+	},
+	'ru-vla': {
+	  code: 'RU-VLA',
+	  name: 'Владимирская область'
+	},
+	'ru-vgg': {
+	  code: 'RU-VGG',
+	  name: 'Волгоградская область'
+	},
+	'ru-vlg': {
+	  code: 'RU-VLG',
+	  name: 'Вологодская область'
+	},
+	'ru-vor': {
+	  code: 'RU-VOR',
+	  name: 'Воронежская область'
+	},
+	'ru-yev': {
+	  code: 'RU-YEV',
+	  name: 'Еврейская автономная область'
+	},
+	'ru-zab': {
+	  code: 'RU-ZAB',
+	  name: 'Забайкальский край'
+	},
+	'ru-iva': {
+	  code: 'RU-IVA',
+	  name: 'Ивановская область'
+	},
+	'ru-irk': {
+	  code: 'RU-IRK',
+	  name: 'Иркутская область'
+	},
+	'ru-kb': {
+	  code: 'RU-KB',
+	  name: 'Кабардино-Балкарская Республика'
+	},
+	'ru-kgd': {
+	  code: 'RU-KGD',
+	  name: 'Калининградская область'
+	},
+	'ru-klu': {
+	  code: 'RU-KLU',
+	  name: 'Калужская область'
+	},
+	'ru-kam': {
+	  code: 'RU-KAM',
+	  name: 'Камчатский край'
+	},
+	'ru-kc': {
+	  code: 'RU-KC',
+	  name: 'Карачаево-Черкесская Республика'
+	},
+	'ru-kem': {
+	  code: 'RU-KEM',
+	  name: 'Кемеровская область - Кузбасс'
+	},
+	'ru-kir': {
+	  code: 'RU-KIR',
+	  name: 'Кировская область'
+	},
+	'ru-kos': {
+	  code: 'RU-KOS',
+	  name: 'Костромская область'
+	},
+	'ru-kda': {
+	  code: 'RU-KDA',
+	  name: 'Краснодарский край'
+	},
+	'ru-kya': {
+	  code: 'RU-KYA',
+	  name: 'Красноярский край'
+	},
+	'ru-kgn': {
+	  code: 'RU-KGN',
+	  name: 'Курганская область'
+	},
+	'ru-krs': {
+	  code: 'RU-KRS',
+	  name: 'Курская область'
+	},
+	'ru-len': {
+	  code: 'RU-LEN',
+	  name: 'Ленинградская область'
+	},
+	'ru-lip': {
+	  code: 'RU-LIP',
+	  name: 'Липецкая область'
+	},
+	'ru-mag': {
+	  code: 'RU-MAG',
+	  name: 'Магаданская область'
+	},
+	'ru-mos': {
+	  code: 'RU-MOS',
+	  name: 'Московская область'
+	},
+	'ru-mur': {
+	  code: 'RU-MUR',
+	  name: 'Мурманская область'
+	},
+	'ru-nen': {
+	  code: 'RU-NEN',
+	  name: 'Ненецкий автономный округ'
+	},
+	'ru-niz': {
+	  code: 'RU-NIZ',
+	  name: 'Нижегородская область'
+	},
+	'ru-ngr': {
+	  code: 'RU-NGR',
+	  name: 'Новгородская область'
+	},
+	'ru-nvs': {
+	  code: 'RU-NVS',
+	  name: 'Новосибирская область'
+	},
+	'ru-oms': {
+	  code: 'RU-OMS',
+	  name: 'Омская область'
+	},
+	'ru-ore': {
+	  code: 'RU-ORE',
+	  name: 'Оренбургская область'
+	},
+	'ru-orl': {
+	  code: 'RU-ORL',
+	  name: 'Орловская область'
+	},
+	'ru-pnz': {
+	  code: 'RU-PNZ',
+	  name: 'Пензенская область'
+	},
+	'ru-per': {
+	  code: 'RU-PER',
+	  name: 'Пермский край'
+	},
+	'ru-pri': {
+	  code: 'RU-PRI',
+	  name: 'Приморский край'
+	},
+	'ru-psk': {
+	  code: 'RU-PSK',
+	  name: 'Псковская область'
+	},
+	'ru-ad': {
+	  code: 'RU-AD',
+	  name: 'Республика Адыгея (Адыгея)'
+	},
+	'ru-al': {
+	  code: 'RU-AL',
+	  name: 'Республика Алтай'
+	},
+	'ru-ba': {
+	  code: 'RU-BA',
+	  name: 'Республика Башкортостан'
+	},
+	'ru-bu': {
+	  code: 'RU-BU',
+	  name: 'Республика Бурятия'
+	},
+	'ru-da': {
+	  code: 'RU-DA',
+	  name: 'Республика Дагестан'
+	},
+	'ru-in': {
+	  code: 'RU-IN',
+	  name: 'Республика Ингушетия'
+	},
+	'ru-kl': {
+	  code: 'RU-KL',
+	  name: 'Республика Калмыкия'
+	},
+	'ru-kr': {
+	  code: 'RU-KR',
+	  name: 'Республика Карелия'
+	},
+	'ru-ko': {
+	  code: 'RU-KO',
+	  name: 'Республика Коми'
+	},
+	'ua-43': {
+	  code: 'UA-43',
+	  name: 'Республика Крым'
+	},
+	'ru-me': {
+	  code: 'RU-ME',
+	  name: 'Республика Марий Эл'
+	},
+	'ru-mo': {
+	  code: 'RU-MO',
+	  name: 'Республика Мордовия'
+	},
+	'ru-sa': {
+	  code: 'RU-SA',
+	  name: 'Республика Саха (Якутия)'
+	},
+	'ru-se': {
+	  code: 'RU-SE',
+	  name: 'Республика Северная Осетия - Алания'
+	},
+	'ru-ta': {
+	  code: 'RU-TA',
+	  name: 'Республика Татарстан (Татарстан)'
+	},
+	'ru-ty': {
+	  code: 'RU-TY',
+	  name: 'Республика Тыва'
+	},
+	'ru-kk': {
+	  code: 'RU-KK',
+	  name: 'Республика Хакасия'
+	},
+	'ru-ros': {
+	  code: 'RU-ROS',
+	  name: 'Ростовская область'
+	},
+	'ru-rya': {
+	  code: 'RU-RYA',
+	  name: 'Рязанская область'
+	},
+	'ru-sam': {
+	  code: 'RU-SAM',
+	  name: 'Самарская область'
+	},
+	'ru-sar': {
+	  code: 'RU-SAR',
+	  name: 'Саратовская область'
+	},
+	'ru-sak': {
+	  code: 'RU-SAK',
+	  name: 'Сахалинская область'
+	},
+	'ru-sve': {
+	  code: 'RU-SVE',
+	  name: 'Свердловская область'
+	},
+	'ru-smo': {
+	  code: 'RU-SMO',
+	  name: 'Смоленская область'
+	},
+	'ru-sta': {
+	  code: 'RU-STA',
+	  name: 'Ставропольский край'
+	},
+	'ru-tam': {
+	  code: 'RU-TAM',
+	  name: 'Тамбовская область'
+	},
+	'ru-tve': {
+	  code: 'RU-TVE',
+	  name: 'Тверская область'
+	},
+	'ru-tom': {
+	  code: 'RU-TOM',
+	  name: 'Томская область'
+	},
+	'ru-tul': {
+	  code: 'RU-TUL',
+	  name: 'Тульская область'
+	},
+	'ru-tyu': {
+	  code: 'RU-TYU',
+	  name: 'Тюменская область'
+	},
+	'ru-ud': {
+	  code: 'RU-UD',
+	  name: 'Удмуртская Республика'
+	},
+	'ru-uly': {
+	  code: 'RU-ULY',
+	  name: 'Ульяновская область'
+	},
+	'ru-kha': {
+	  code: 'RU-KHA',
+	  name: 'Хабаровский край'
+	},
+	'ru-khm': {
+	  code: 'RU-KHM',
+	  name: 'Ханты-Мансийский автономный округ - Югра'
+	},
+	'ru-che': {
+	  code: 'RU-CHE',
+	  name: 'Челябинская область'
+	},
+	'ru-ce': {
+	  code: 'RU-CE',
+	  name: 'Чеченская Республика'
+	},
+	'ru-cu': {
+	  code: 'RU-CU',
+	  name: 'Чувашская Республика - Чувашия'
+	},
+	'ru-chu': {
+	  code: 'RU-CHU',
+	  name: 'Чукотский автономный округ'
+	},
+	'ru-yan': {
+	  code: 'RU-YAN',
+	  name: 'Ямало-Ненецкий автономный округ'
+	},
+	'ru-yar': {
+	  code: 'RU-YAR',
+	  name: 'Ярославская область'
+	},
+	'ru-mow': {
+	  code: 'RU-MOW',
+	  name: 'город Москва'
+	},
+	'ru-spe': {
+	  code: 'RU-SPE',
+	  name: 'город Санкт-Петербург'
+	},
+	'ua-40': {
+	  code: 'UA-40',
+	  name: 'город Севастополь'
+	},
+	'ru-lnr': {
+	  code: 'RU-LNR',
+	  name: 'Луганская народная республика'
+	},
+	'ru-dnr': {
+	  code: 'RU-DNR',
+	  name: 'Донецкая народная республика'
+	},
+	'ua-23': {
+	  code: 'UA-23',
+	  name: 'Запорожская область'
+	},
+	'ua-65': {
+	  code: 'UA-65',
+	  name: 'Херсонская область'
 	}
-}
+  }
+  
