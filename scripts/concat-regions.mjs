745bf7d17c7da700d932bff1020dc29fa3e55c35
Boris Panchenko <oberon@list.ru>
2022-11-09 01:26:28 +0300

    concatenation all regions presented into source to one file

---
+++ scripts/concat-regions.mjs
@@ -0,0 +1,40 @@
+import properties, { defaultProps } from "../source/geo/properties.js"
+
+import fs from "fs"
+import path from "path"
+import pick from "lodash/pick.js"
+
+const EXT = '.geojson'
+const SOURCE = path.resolve(process.cwd(), './source/geo')
+const DIST = path.resolve(process.cwd(), './assets/russia-regions.geojson')
+
+const geojson = {
+	type: 'FeatureCollection',
+	features: []
+}
+
+// collecting features
+
+const filenames = fs.readdirSync(SOURCE)
+
+filenames.forEach(filename => {
+	if (path.extname(filename) == EXT) {
+		let code = path.parse(filename).name
+		let props = properties[code] ?? defaultProps
+
+		let rawdata = fs.readFileSync(path.join(SOURCE, filename))
+		let feature = JSON.parse(rawdata)
+
+		feature.properties = {
+			...props,
+			...pick(feature.properties, Object.keys(props))
+		}
+
+		geojson.features.push(feature)
+	}
+})
+
+// writing
+
+let rawdata = JSON.stringify(geojson)
+fs.writeFileSync(DIST, rawdata)
