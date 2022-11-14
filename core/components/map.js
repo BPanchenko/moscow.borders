182cc9b37d7916e6c5b57115f6af9b1d0fe9698a
Boris Panchenko <oberon@list.ru>
2022-11-14 16:09:02 +0300

    using different projections

--- core/components/map.js
+++ core/components/map.js
@@ -1,6 +1,7 @@
-import { geoAlbers, geoPath, select } from "d3"
+import { geoAlbers, geoMercator, geoPath, select } from "d3"
 
 import EventEmmiter from "protosite-core/lib/event-emmiter.js"
+import findKey from "lodash/findKey.js"
 import isEmpty from "lodash/isEmpty.js"
 
 /* CSS Class Names
@@ -9,6 +10,38 @@ import isEmpty from "lodash/isEmpty.js"
 const CLS = Object.freeze({
 	container: 'c-map',
 	svg: 'c-map__plot'
+})
+
+/* Projections Adaptor
+ ========================================================================== */
+
+const PROJECTIONIS = Object.freeze({
+	albers: {
+		default: true,
+		init: () => (
+			geoAlbers()
+				.rotate([-105, 0])
+				.center([-10, 65])
+				.parallels([52, 64])
+		),
+		setup: (projection, w, h) => (
+			projection
+				.scale(610)
+				.translate([w/2, h/2])
+		)
+	},
+	mercator: {
+		init: () => (
+			geoMercator()
+				.rotate([-11, 0])
+				.center([90, 70])
+		),
+		setup: (projection, w, h) => (
+			projection
+				.scale(w / (1.15 * Math.PI))
+				.translate([w/2, h/2])
+		)
+	}
 })
 
 /* GeoJSON Data Container
@@ -69,25 +102,23 @@ class MapComponentElement extends HTMLElement {
 
 	constructor() {
 		super()
-		
+		this.projection.init()
 		this.#data = new DataProvider({ ...this.dataset })
 		this.#data.on('sync', () => this.update())
-
-		this.#projection = geoAlbers()
-			.rotate([-105, 0])
-			.center([-10, 65])
-			.parallels([52, 64])
-
-		this.#path = geoPath().projection(this.#projection)
-
 		this.#resizeObserver = new ResizeObserver(entries => this.update())
 	}
 
 	attributeChangedCallback(name, previos, value) {
+		console.log(name)
 		switch (name) {
+			case 'data-projection':
+				this.projection.init()
+				this.update()
+				break
 			case 'data-url':
 				this.#data.url = value
 				this.#data.fetch()
+				break
 		}
 	}
 
@@ -101,25 +132,48 @@ class MapComponentElement extends HTMLElement {
 	}
 
 	update() {
-		this.#height = this.offsetHeight
 		this.#width = this.offsetWidth
+		this.#height = this.offsetHeight
 		this.#svg.attr('width', this.#width).attr('height', this.#height)
 
-		this.#projection
-			.scale(610)
-			.translate([
-				this.#width/2,
-				this.#height/2
-			])
+		this.projection.setup()
 
 		this.#svg.selectAll('path')
+			.remove()
+		
+		this.#svg.selectAll('path')
 			.data(this.#data.features)
-			.enter().append('path')
+			.enter()
+			.append('path')
 			.attr('d', this.#path)
 	}
 
+	get projection() {
+		let name = this.dataset.projection
+		if (!PROJECTIONIS.hasOwnProperty(name)) {
+			name = findKey(PROJECTIONIS, p => p.default)
+			this.dataset.projection = name
+		}
+		let { init, setup } = PROJECTIONIS[name]
+		return {
+			name,
+			instance: this.#projection,
+			init: () => {
+				this.#projection = init()
+				this.#path = geoPath().projection(this.#projection)
+			},
+			setup: () => {
+				setup(
+					this.#projection,
+					this.#width,
+					this.#height
+				)
+			}
+		}
+	}
+
 	static get observedAttributes() {
-		return ['data-url']
+		return ['data-projection', 'data-url']
 	}
 }
 
